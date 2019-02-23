7f00855e6d26a0bf1c03c19a02f8578568399f6e
shyamprakash34 <shyamprakashtesting@gmail.com>
2019-02-23 16:41:52 +0530

    initialupload

---
+++ src/test/java/First_pp/Jenkins_first/AppTest.java
@@ -0,0 +1,13 @@
+package First_pp.Jenkins_first;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	@Test
+    public void testB() {
+    	System.out.println("Hello test b");
+    }
+}
