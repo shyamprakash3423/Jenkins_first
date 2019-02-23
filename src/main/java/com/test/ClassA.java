feca55eb3f68b5f7f4441861181ab3ab65f6fd9f
shyamprakash34 <shyamprakashtesting@gmail.com>
2019-02-23 17:05:54 +0530

    selenium commit

--- src/main/java/com/test/ClassA.java
+++ src/main/java/com/test/ClassA.java
@@ -1,5 +1,16 @@
 package com.test;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
 public class ClassA {
+	@Test
+	 public void testB() {
+	    	System.out.println("Hello test b");
+	    	System.setProperty("webdriver.firefox.marionette", "D:\\shyam\\set ups\\geckodriver.exe");
+	    	WebDriver driver=new FirefoxDriver();
+	    	driver.get("https:\\www.google.com");
+	    }
 
 }
